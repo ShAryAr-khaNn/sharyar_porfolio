f60332390e2c3104287e64e3a82002c0da56d996
sharyark <ksharyar12345@gmail.com>
2025-04-03 19:37:24 +0300

    site map

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({ hostname: 'https://yourwebsite.com' }) // Replace with your domain
+  ]
+});
